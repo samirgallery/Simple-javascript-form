4c0c2231dad33acf16157e5401919e8ba41bc5f6
Samir Pihul <90954274+samirgallery@users.noreply.github.com>
2022-08-26 13:16:46 +0530

    commit

---
+++ userlogin.js
@@ -0,0 +1,26 @@
+const uid="Leancodeonline 123";
+var fullName ="Samir";
+var lastName ="Pihul"
+var email="pihulsamir@gmail.com";
+var password="1234455";
+var confirmPassword="1234455";
+var country ="India"
+var state ="Maharastra";
+var freeCourseCount=12;
+var userLoggedInGoogle=false;
+var userLoggedInfacebook=true;
+
+console.log(`
+    Unique Id:${uid}
+    user Full Name:${fullName}
+    User  Last Name;${lastName}
+    User  Email;${email}
+    User  Password;${password}
+    User  Country;${country}
+    User  State;${state}
+    User  CourseCount;${freeCourseCount}
+    user  Logged In Google:${userLoggedInGoogle}
+    user Logged In facebook:${userLoggedInfacebook}
+    
+
+`)
